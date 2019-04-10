27a32db561840975ef9226e256826a0c4e610624
Ignacio <nachootegui@hotmail.com>
2019-04-10 10:58:15 -0300

    Scripting & Lvl Dessign
    Rotacion de torres a objetivos y comenzando con los disparos

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 70f;
+    private Transform target;
+
+
+
+
+    public void Seek(Transform _target)
+    {
+        target = _target;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Vector3 dir = target.position - transform.position;
+        float distanceThisFrame = speed * Time.deltaTime;
+        if (dir.magnitude <= distanceThisFrame)
+        {
+            HitTarget();
+            return;
+        }
+        transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+    }
+
+
+    void HitTarget()
+    {
+        Debug.Log("We hit something!");
+    }
+}
